9b05fcc07322e06e1eae6d20413b220ab0937b7e
Jay Patel <pjay20301@gmail.com>
2021-02-08 11:17:28 +0530

    Database and Migrations Done

--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-
-posts = [
-    {
-        'author': 'Jay Patel',
-        'title': 'Blog Post 1',
-        'content': 'First Post content',
-        'date_posted': 'February 7, 2021'
-    },
-    {
-        'author': 'Vraj Gandhi',
-        'title': 'Blog Post 2',
-        'content': 'Second Post content',
-        'date_posted': 'February 7, 2021'
-    },
-    {
-        'author': 'Kruti Baraiya',
-        'title': 'Blog Post 3',
-        'content': 'Third Post content',
-        'date_posted': 'February 7, 2021'
-    },
-    {
-        'author': 'Janhvi Bahuguna',
-        'title': 'Blog Post 4',
-        'content': 'Fourth Post content',
-        'date_posted': 'February 7, 2021'
-    }
-]
 
 def home(request):
 	context = {
-	    'posts': posts
+	    'posts': Post.objects.all()
 	}
 	return render(request,'blog/home.html', context)
 
